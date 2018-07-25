78f205604348183a8a701deb0e09fcfce68c65ff
saeromcho <1perunregret@gmail.com>
2018-07-25 16:45:02 +0900

    react UI Framework Sematic-UI 모듈 설치 및 app.js 코드 일부 수정

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,15 @@
 'use strict';
 
 // tag::vars[]
-const React = require('react');
-const ReactDOM = require('react-dom');
-const client = require('./client');
+// const React = require('react');
+// const ReactDOM = require('react-dom');
+// const client = require('./client');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import client from'./client';
+import { Menu } from 'semantic-ui-react';
+// 메인 인포 타임라인 마이페이지
 // end::vars[]
 
 // tag::app[]
@@ -14,7 +20,16 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {users: []};
+        this.state = {
+            users: [], 
+            activeItem: ''
+        };
+
+        this.handleItemClick = this.handleItemClick.bind(this);
+    }
+
+    componentWillMount() {
+        document.addEventListener("handleItemClick", this.handleItemClick.bind(this));
     }
 
     componentDidMount() {
@@ -24,9 +39,41 @@ class App extends React.Component {
         });
     }
 
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+
     render() {
+        const {activeItem} = this.state.activeItem;
         return (
-            <UserList users={this.state.users}/>
+            <div>
+                <UserList users={this.state.users}/>
+                <Menu>
+                    <Menu.Item
+                        name='main'
+                        active={activeItem === 'main'}
+                        content='Main_메인페이지'
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='Information'
+                        active={activeItem === 'Information'}
+                        content='Infomation_정보'
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='TimeLine'
+                        active={activeItem === 'TimeLine'}
+                        content='TimeLine_타임라인'
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='MyPage'
+                        active={activeItem === 'MyPage'}
+                        content='MyPage_마이페이지'
+                        onClick={this.handleItemClick}
+                    />
+                </Menu>
+            </div>
         );
     }
 }
