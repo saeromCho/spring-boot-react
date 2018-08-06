b7de75fa9320198ba387a5d105d6709e1ef2efc2
bongkim <bongkim@sdmalt.com>
2018-08-06 20:20:40 +0900

    rename

--- run-standalone.sh
+++ run-standalone.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name spring-boot-react -p 8081:80 -v $(pwd)/src/html:/usr/share/nginx/html -d colligence-io/spring-boot-react  
+docker run --name alpha.colligence.io -p 80:80 -p 443:443 -v $(pwd)/src/html:/usr/share/nginx/html -d colligence-io 
